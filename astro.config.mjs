90e7ffaa3dda28c5dc98feb95ec0045420326211
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-05-03 03:45:54 -0500

    Feat - vercel configuration, cloudflare fail

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,24 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
+// import cloudflare from '@astrojs/cloudflare';
 
 import db from '@astrojs/db';
+
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-
   output: "static",
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-  }),
+
+  // adapter: cloudflare({
+  //   imageService: 'cloudflare',
+  // }),
   integrations: [mdx(), sitemap(), db()],
   security: {
     checkOrigin: false
-  }
+  },
+
+  adapter: vercel()
 });
