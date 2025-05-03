07d63a442b90b1ea31eb2a9b4b491dd11a17f8f1
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-05-03 04:45:39 -0500

    Feat - Post table added

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,5 @@
-import { Clients, db } from 'astro:db';
+import { getCollection } from 'astro:content';
+import { Clients, db, Posts } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -15,7 +16,18 @@ export default async function seed() {
 	]);
 
 
-	// TODO
+	const posts = await getCollection('blog');
+
+
+	await db.insert(Posts).values(
+		posts.map(post => ({
+			id: post.id,
+			title: post.data.title,
+			likes: Math.round(Math.random() * 100),
+		}))
+	)
+
+
 
 	console.log('seed executed');
 
