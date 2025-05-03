b646788e3d30e866d022e2784bce13bb37649bef
alanlb195 <alanlopezbarrientos@live.com.mx>
2025-05-03 04:54:52 -0500

    HotFix - Not working on vercel so, adapter changed to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-// import cloudflare from '@astrojs/cloudflare';
-
 import db from '@astrojs/db';
 
-import vercel from '@astrojs/vercel';
+
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   output: "static",
-
-  // adapter: cloudflare({
-  //   imageService: 'cloudflare',
-  // }),
   integrations: [mdx(), sitemap(), db()],
+
   security: {
     checkOrigin: false
   },
 
-  adapter: vercel()
+  adapter: netlify(),
 });
